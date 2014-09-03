4a5b158792d15268fd56d50c95a3137637a7be7c
Joon Min <joongi.min@gmail.com>
2014-09-03 17:17:47 -0400

    jQuery 2.1.1로 업그레이드

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery-2.1.1
 //= require jquery_ujs
 //= require ./app
 //= require_tree ./app
